176657aeab63f5a8dbc7fb2bf7639dc3b97edae0
Pedro Marques <pedro.dm.marques@gmail.com>
2018-05-24 11:05:46 +0100

    added new-aere es-index

--- index_schemas/new-aere.py
+++ index_schemas/new-aere.py
@@ -8,13 +8,17 @@ def getSchema():
 		},
 
 		"mappings": {
-			"aere": {
+			"new-aere": {
 				"properties": {
 					"timestamp": { "type": "date" },
 					"ipMapLocation": { "type": "geo_point" },
 
-					"arcane_reported": { "type": "boolean" },
-					"distil_reported": { "type": "boolean" },
+					"distil_label": { "type": "boolean" },
+					"arcane_label": { "type": "boolean" },
+					"manual_label": { "type": "boolean" },
+
+					#"arcane_reported": { "type": "boolean" },
+					#"distil_reported": { "type": "boolean" },
 
 					"epoch": { "type": "long" },
 					"dcxID": { "type": "keyword" },
